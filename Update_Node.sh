81b9b6bafbf8383d28b7d90758823cc58eaa8036
WallanceProject <56075299+WallanceProject@users.noreply.github.com>
2019-12-10 10:48:11 +0100

    Update Update_Node.sh

--- Update_Node.sh
+++ Update_Node.sh
@@ -8,11 +8,11 @@
 # Copy Key to target: ssh-copy-id pi@ipaddr
 
 # Find All Node IPs
-IPs=$(sudo nmap -sn $(ip -o -f inet addr show | awk '{print $4}' | grep '192') | awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " => "$3;}' | grep "B8:27:EB" | awk '{print $1}')
+IPs=$(sudo nmap -n -sn $(ip -o -f inet addr show | awk '{print $4}' | grep '192') | awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " => "$3;}' | grep "B8:27:EB" | awk '{print $1}')
 
 for ip in $IPs
 do
 	echo "********** Update Node $ip **********"
 	scp WallanceDDS_Node.zip pi@$ip:/home/pi/
-	ssh pi@$ip 'unzip WallanceDDS_Node.zip && sudo ./Install_WallanceDDS_Node.sh'
+	ssh pi@$ip 'unzip WallanceDDS_Node.zip && ./Install_WallanceDDS_Node.sh'
 done
